0121967dc235223b135801c66538b5e5087dc59d
masedri <marck.rodriguez09@gmail.com>
2023-04-12 01:14:24 +0200

    feat: add dynamic routes podcastById and episodeById

---
+++ src/pages/podcast/[id]/episode/[episodeId].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router'
+
+export default function PodcastById() {
+  const router = useRouter()
+  const { episodeId } = router.query
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Episode {episodeId}
+    </main>
+  )
+}
