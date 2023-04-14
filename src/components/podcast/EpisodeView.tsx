3ccd1b9191b64c0879b211e114dd09df68a419f0
masedri <marck.rodriguez09@gmail.com>
2023-04-14 02:08:13 +0200

    feat: add EpisodeView, EpisodeById Page, audio player, filter podcast

---
+++ src/components/podcast/EpisodeView.tsx
@@ -0,0 +1,42 @@
+import { Card } from '@/components/Card'
+import { EpisodeById } from '@/features/podcast/types'
+import Link from 'next/link'
+
+export const EpisodeView = ({ podcastById }: { podcastById: EpisodeById[] }) => {
+  return (
+    <div className="flex flex-col gap-5 w-full">
+      <Card className="p-3">
+        <div className="text-2xl font-sans font-bold">Episodes: {podcastById[0]?.numberOfEpisodes}</div>
+      </Card>
+      <Card className="p-5 h">
+        <section className="flex justify-between p-3 font-bold border-b-4">
+          <div>Title</div>
+          <div className="flex justify-between w-1/3">
+            <div>Date</div>
+            <div>Duration</div>
+          </div>
+        </section>
+        <ul className="flex flex-col w-full divide-y-2 overflow-y-auto max-h-[900px] scroll-smooth  ">
+          {podcastById.map((episode, index) => {
+            return (
+              <li
+                key={episode.trackId}
+                className={`flex flex-row w-full justify-between items-center p-3 ${
+                  index % 2 === 0 && `bg-slate-100`
+                } hover:bg-cyan-800 group`}
+              >
+                <Link href={{ pathname: episode.link, query: { ...episode } }}>
+                  <p className="w-56 lg:w-72 xl:w-96 2xl:w-full  text-cyan-700 group-hover:text-white">{episode.trackName}</p>
+                </Link>
+                <div className="flex w-1/3 justify-between group-hover:text-white gap-5">
+                  <p>{episode.releaseDate}</p>
+                  <p>{episode.trackTimeMillis}</p>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </Card>
+    </div>
+  )
+}
--- src/pages/podcast/[id]/episode/[episodeId].tsx
+++ src/pages/podcast/[id]/episode/[episodeId].tsx
@@ -1,12 +1,32 @@
 import { useRouter } from 'next/router'
+import { Container } from '@/components/containers'
+
+import { PodcastCardDetail } from '@/components/podcast/PodcastCardDetail'
+import { Card } from '@/components/Card'
+import { sanitaze } from '@/helpers'
+import ReactAudioPlayer from 'react-audio-player'
 
 export default function EpisodeById() {
   const router = useRouter()
-  const { episodeId } = router.query
+  const { trackName, description, episodeUrl } = router.query
+  const cleanHTML = sanitaze(description as string)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Episode {episodeId}
-    </main>
+    <Container className="flex lg:flex-row lg:items-start flex-col items-center gap-20">
+      <PodcastCardDetail></PodcastCardDetail>
+      <Card className="flex flex-col gap-5 p-5 w-full">
+        <div className="text-xl font-bold capitalize">{trackName}</div>
+        <p
+          className="whitespace-pre-line max-h-96 overflow-y-auto"
+          dangerouslySetInnerHTML={{ __html: cleanHTML }}
+        ></p>
+        {/* wrapper html audio */}
+        <ReactAudioPlayer
+          className="w-full "
+          src={episodeUrl as string}
+          controls
+        ></ReactAudioPlayer>
+      </Card>
+    </Container>
   )
 }
